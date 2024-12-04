60f5c854157ad7e8e94aeb88f4a4b0f1f27d4a6b
Beihaisir <103311481+Beihaisir@users.noreply.github.com>
2024-12-04 16:44:05 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,11 +51,11 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '	85qAW8l6NJ3FPE3AjY3d97Bpnnc2JU9oN39YqiHcpsE',
+  CALLBACK_TEMPLATE_ID: '85qAW8l6NJ3FPE3AjY3d97Bpnnc2JU9oN39YqiHcpsE',
 
   CALLBACK_USERS: [
     {
-      name: '自己',
+      name: '小a宝贝',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oeiXl6t6lyTQZkpOZme9PfdAzwjw',
     }
